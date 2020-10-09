b8f603459cdfe9dee4b6aa339d9620daa26eb226
andrea2025 <anitaogechi9@gmail.com>
2020-10-09 11:13:26 +0100

    created login and register page

---
+++ app/src/main/java/com/example/edu24/RegisterActivity.java
@@ -0,0 +1,37 @@
+package com.example.edu24;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+Button mRegisterBtn;
+TextView mLoginText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mRegisterBtn = findViewById(R.id.registerBtn);
+        mLoginText = findViewById(R.id.loginTxt);
+        mLoginText.setOnClickListener(this);
+        mRegisterBtn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.registerBtn:
+                Intent intent = new Intent(this,LoginActivity.class);
+                startActivity(intent);
+            case R.id.loginTxt:
+                Intent loginIntent = new Intent(this,LoginActivity.class);
+                startActivity(loginIntent);
+        }
+    }
+}
